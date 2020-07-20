863360cd33b8006b62d24ec0238e8343c4108387
Athos Nochi <athosnochi@gmail.com>
2020-07-20 19:54:01 -0300

    jj

--- config/app.php
+++ config/app.php
@@ -105,7 +105,7 @@ return [
     */
 
     'key' => env('APP_KEY',
-    'SomeRandomString'),
+    'SomeRandomStringSomeRandomStringSomeRandomStringSomeRandomString'),
 
     'cipher' => 'AES-256-CBC',
 
